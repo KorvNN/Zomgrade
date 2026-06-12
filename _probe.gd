e4a0d980bb69022c155ad5a3ba62555b99907ff6
Efe Körün <efekorun01@hotmail.com>
2026-06-12 23:58:38 +0300

    x

--- _probe.gd
+++
@@ -1,6 +0,0 @@
-extends SceneTree
-func _initialize() -> void:
-	for p in ["res://scripts/game.gd", "res://scripts/dungeon_level.gd"]:
-		var r = load(p)
-		print("OK " if r != null else "FAIL ", p)
-	quit()
